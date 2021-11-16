f4624f103d0e936bd3f68fafded15d64c05be8e6
Darshan-Movilitas <darshan.pithadiya@movilitas.com>
2021-11-16 12:19:38 +0000

    Revert "Initialization"
    This reverts commit 99538e22e589d5b2e897d9481006f0eeae62cac4.

--- serviceBinding.js
+++
@@ -1,5 +0,0 @@
-function initModel() {
-	var sUrl = "/FEBE-MII-DEV/XMII/MIIODataV2/";
-	var oModel = new sap.ui.model.odata.ODataModel(sUrl, true);
-	sap.ui.getCore().setModel(oModel);
-}
--- webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"FujiFilm/Batch_Data_Formulation/test/unit/controller/Main.controller"
-], function () {
-	"use strict";
-});
